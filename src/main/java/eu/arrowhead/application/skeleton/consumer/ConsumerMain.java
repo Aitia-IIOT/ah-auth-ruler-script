dd033d8470dfabe1b26ccd86f0878d57590b3fa5
borditamas <borditamas@gmail.com>
2024-06-19 10:16:48 +0200

    Init project from ah consumer skelet

---
+++ src/main/java/eu/arrowhead/application/skeleton/consumer/ConsumerMain.java
@@ -0,0 +1,48 @@
+package eu.arrowhead.application.skeleton.consumer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpMethod;
+
+import ai.aitia.arrowhead.application.library.ArrowheadService;
+import eu.arrowhead.common.CommonConstants;
+import eu.arrowhead.common.dto.shared.OrchestrationFlags.Flag;
+import eu.arrowhead.common.dto.shared.OrchestrationFormRequestDTO;
+import eu.arrowhead.common.dto.shared.OrchestrationFormRequestDTO.Builder;
+import eu.arrowhead.common.dto.shared.OrchestrationResponseDTO;
+import eu.arrowhead.common.dto.shared.OrchestrationResultDTO;
+import eu.arrowhead.common.dto.shared.ServiceQueryFormDTO;
+import eu.arrowhead.common.exception.ArrowheadException;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {CommonConstants.BASE_PACKAGE, "ai.aitia"}) //TODO: add custom packages if any
+public class ConsumerMain implements ApplicationRunner {
+    
+    //=================================================================================================
+	// members
+	
+    @Autowired
+	private ArrowheadService arrowheadService;
+    
+	private final Logger logger = LogManager.getLogger( ConsumerMain.class );
+    
+    //=================================================================================================
+	// methods
+
+	//------------------------------------------------------------------------------------------------
+    public static void main( final String[] args ) {
+    	SpringApplication.run(ConsumerMain.class, args);
+    }
+
+    //-------------------------------------------------------------------------------------------------
+    @Override
+	public void run(final ApplicationArguments args) throws Exception {
+		System.out.println("NOT IMPLEMENTED YET!");
+	}
+}
