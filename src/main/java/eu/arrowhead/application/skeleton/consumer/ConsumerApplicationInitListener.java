a257d51c77fc1e424b8ddb8cf96b081b16ce322d
borditamas <borditamas@gmail.com>
2024-06-20 12:02:32 +0200

    Simplify AppInitListener

--- src/main/java/eu/arrowhead/application/skeleton/consumer/ConsumerApplicationInitListener.java
+++ src/main/java/eu/arrowhead/application/skeleton/consumer/ConsumerApplicationInitListener.java
@@ -1,26 +1,14 @@
 package eu.arrowhead.application.skeleton.consumer;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
-import ai.aitia.arrowhead.application.library.ArrowheadService;
 import ai.aitia.arrowhead.application.library.config.ApplicationInitListener;
 import eu.arrowhead.common.core.CoreSystem;
 
 @Component
 public class ConsumerApplicationInitListener extends ApplicationInitListener {
 
-	//=================================================================================================
-	// members
-	
-	@Autowired
-	private ArrowheadService arrowheadService;
-	
-	private final Logger logger = LogManager.getLogger(ConsumerApplicationInitListener.class);
-	
 	//=================================================================================================
 	// methods
 
@@ -32,17 +20,5 @@ public class ConsumerApplicationInitListener extends ApplicationInitListener {
 		checkCoreSystemReachability(CoreSystem.SERVICEREGISTRY);
 		checkCoreSystemReachability(CoreSystem.AUTHORIZATION);
 		
-		
-		//Initialize Arrowhead Context
-		arrowheadService.updateCoreServiceURIs(CoreSystem.AUTHORIZATION);
-
-		//TODO: implement here any custom behavior on application start up
-		
-	}
-	
-	//-------------------------------------------------------------------------------------------------
-	@Override
-	public void customDestroy() {
-		//TODO: implement here any custom behavior on application shutdown
 	}
 }
